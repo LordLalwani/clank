ccaac49db52d23663916c5bf8aad91356b5e1daa
LordLalwani <Deepplalwani@hotmail.com>
2020-08-26 22:09:19 +1000

    Authentication (sign-in): partially finished

--- src/App.js
+++ src/App.js
@@ -1,120 +1,17 @@
 /* src/App.js */
-import React, { useEffect, useState } from 'react'
-import Amplify, { API, graphqlOperation, Auth } from 'aws-amplify'
-import { createTodo, deleteTodo } from './graphql/mutations'
-import { listTodos } from './graphql/queries'
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-
+import Amplify from 'aws-amplify';
+import React from 'react';
 import awsExports from "./aws-exports";
+import SignIn from './modals/signIn';
+
 Amplify.configure(awsExports);
 
-const initialState = { name: '', description: '', age: '' }
-
 const App = () => {
-  const [formState, setFormState] = useState(initialState)
-  const [todos, setTodos] = useState([])
-
-  const getCurrentUser = async () => {
-    let user
-    try {
-      user = await Auth.currentAuthenticatedUser();
-    } catch (err) {
-      console.log("cant get current user", err)
-    }
-    return user
-  }
-
-  useEffect(() => {
-    fetchTodos()
-  })
-
-  function setInput(key, value) {
-    setFormState({ ...formState, [key]: value })
-  }
-
-  async function fetchTodos() {
-    try {
-      const user = await getCurrentUser()
-      const userSub = user.attributes.sub
-
-      const todoData = await API.graphql(graphqlOperation(listTodos, {
-        filter: {
-          createdBy: {
-            eq: userSub
-          }
-        }
-      }))
-
-      const todos = todoData.data.listTodos.items
-      setTodos(todos)
-    } catch (err) { console.log('error fetching todos', err) }
-  }
-
-  async function addTodo() {
-    try {
-      const user = await getCurrentUser()
-      const userSub = user.attributes.sub
-      if (!formState.name || !formState.description || !formState.age) return
-      const todo = { ...formState, createdBy: userSub }
-      setTodos([...todos, todo])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, { input: todo }))
-    } catch (err) {
-      console.log('error creating todo:', err)
-    }
-  }
-
-  const deleteTodoCall = async (e, todo) => {
-    try {
-      await API.graphql(graphqlOperation(deleteTodo, { input: { id: todo.id } }))
-    } catch (err) {
-      console.log('error deleting todo:', err)
-    }
-  }
   return (
-    <div style={styles.container}>
-    <AmplifySignOut/>
-      <h2>Amplify Todos</h2>
-      <input
-        onChange={event => setInput('name', event.target.value)}
-        style={styles.input}
-        value={formState.name}
-        placeholder="Name"
-      />
-      <input
-        onChange={event => setInput('description', event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <input
-        onChange={event => setInput('age', event.target.value)}
-        style={styles.input}
-        value={formState.Age}
-        placeholder="Age"
-      />
-      <button style={styles.button} onClick={addTodo}>Create Todo</button>
-      {
-        todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
-            <p>{todo.age}</p>
-            <button onClick={e => deleteTodoCall(e, todo)}>delete</button>
-          </div>
-        ))
-      }
+    <div>
+      <SignIn />
     </div>
   )
 }
 
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: { marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
-}
-
-export default withAuthenticator(App)
+export default App
---
+++ src/modals/signIn.js
@@ -0,0 +1,143 @@
+import Avatar from '@material-ui/core/Avatar';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import PropTypes from 'prop-types';
+import { default as React } from 'react';
+import { Auth } from 'aws-amplify';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Clank
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const styles = theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+});
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.emailRef = React.createRef();
+        this.passwordRef = React.createRef();
+      }
+
+    handleSignIn = async (event) => {
+        event.preventDefault();
+        const email = this.emailRef.current.value
+        const password = this.passwordRef.current.value
+        
+        try {
+            if(email && password) {
+                const user = await Auth.signIn(email, password);
+                console.log(user)
+            }
+        } catch (error) {
+            console.log('error signing in', error);
+        }
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Welcome To Crank
+        </Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                           inputRef={this.emailRef}
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                         inputRef={this.passwordRef}
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={(e) => (this.handleSignIn(e))}
+                        >
+                            Sign In
+          </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+              </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+                <Box mt={8}>
+                    <Copyright />
+                </Box>
+            </Container>
+        )
+    }
+}
+SignIn.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SignIn);
