29ae92f1ff21e02dab305564ac8b6fec8accff8d
LordLalwani <Deepplalwani@hotmail.com>
2020-09-08 20:21:20 +1000

    Added prettier so we don't get tech acne

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Amplify from 'aws-amplify';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import awsExports from './aws-exports';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Amplify from "aws-amplify";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import awsExports from "./aws-exports";
 
 Amplify.configure(awsExports);
 
@@ -12,7 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
